e203410e73db2c714c453c2295e1be0a7b8dd485
xiaozi0lei <xiaozi0lei@gmail.com>
2018-01-25 12:42:31 +0800

    add spring cloud config to support the external configuration

--- note-service/src/main/java/cn/sunguolei/blog/noteservice/controller/NoteController.java
+++ note-service/src/main/java/cn/sunguolei/blog/noteservice/controller/NoteController.java
@@ -1,6 +1,8 @@
 package cn.sunguolei.blog.noteservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,13 @@ public class NoteController {
     @GetMapping("/index")
     public String index() {
         return "note test";
+    }
+
+    @Value("${spring.datasource.url}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
     }
 }
